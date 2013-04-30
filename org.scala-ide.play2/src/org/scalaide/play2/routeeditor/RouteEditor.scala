fd89fe60d2688f38ce2a19994e6c19e2a1ff7e45
Mirco Dotta <mirco.dotta@typesafe.com>
2013-04-30 15:50:31 +0200

    Extracted `RoutePresentationReconciler`

--- org.scala-ide.play2/src/org/scalaide/play2/routeeditor/RouteEditor.scala
+++ org.scala-ide.play2/src/org/scalaide/play2/routeeditor/RouteEditor.scala
@@ -1,27 +1,28 @@
 package org.scalaide.play2.routeeditor
 
-import org.eclipse.jface.util.PropertyChangeEvent
-import org.eclipse.ui.editors.text.TextEditor
-import org.scalaide.play2.PlayPlugin
-import org.eclipse.ui.texteditor.ChainedPreferenceStore
-import org.eclipse.ui.editors.text.EditorsUI
-import org.eclipse.ui.texteditor.TextOperationAction
-import org.eclipse.jface.text.source.ISourceViewer
-import scala.tools.eclipse.ScalaProject
-import org.eclipse.ui.part.FileEditorInput
 import scala.tools.eclipse.ScalaPlugin
+import scala.tools.eclipse.ScalaProject
 import scala.tools.eclipse.logging.HasLogger
 
-class RouteEditor extends TextEditor with HasLogger {
-  lazy val preferenceStore = new ChainedPreferenceStore(Array(PlayPlugin.preferenceStore, EditorsUI.getPreferenceStore))
-  this.setPreferenceStore(preferenceStore)
-  val sourceViewConfiguration = new RouteConfiguration(preferenceStore, this)
-  setSourceViewerConfiguration(sourceViewConfiguration);
-  setDocumentProvider(new RouteDocumentProvider());
+import org.eclipse.jface.text.source.ISourceViewer
 
-  /**
-   * It is necessary for binding ctrl+shift+f to formatting
-   */
+import org.eclipse.jface.util.PropertyChangeEvent
+import org.eclipse.ui.editors.text.EditorsUI
+import org.eclipse.ui.editors.text.TextEditor
+import org.eclipse.ui.part.FileEditorInput
+import org.eclipse.ui.texteditor.ChainedPreferenceStore
+import org.eclipse.ui.texteditor.TextOperationAction
+import org.scalaide.play2.PlayPlugin
+
+class RouteEditor extends TextEditor with HasLogger {
+  private lazy val preferenceStore = new ChainedPreferenceStore(Array(PlayPlugin.preferenceStore, EditorsUI.getPreferenceStore))
+  private val config = new RouteConfiguration(preferenceStore, this)
+
+  this.setPreferenceStore(preferenceStore)
+  setSourceViewerConfiguration(config)
+  setDocumentProvider(new RouteDocumentProvider())
+
+  /** It is necessary for binding ctrl+shift+f to formatting */
   override def initializeKeyBindingScopes() {
     setKeyBindingScopes(Array("org.scala-ide.play2.routeeditor.editorScope"))
   }
@@ -34,11 +35,10 @@ class RouteEditor extends TextEditor with HasLogger {
     val formatAction = new TextOperationAction(EditorMessages.resourceBundle, "Editor.Format.", this, ISourceViewer.FORMAT)
     formatAction.setActionDefinitionId("org.scala-ide.play2.routeeditor.commands.format")
     setAction("format", formatAction)
-
   }
 
   override def handlePreferenceStoreChanged(event: PropertyChangeEvent) = {
-    sourceViewConfiguration.handlePropertyChangeEvent(event)
+    config.handlePropertyChangeEvent(event)
     super.handlePreferenceStoreChanged(event)
   }
 
@@ -49,8 +49,7 @@ class RouteEditor extends TextEditor with HasLogger {
 
   def getViewer: ISourceViewer = getSourceViewer
   
-  /** Returns project containing the edited file, if it exists.
-   */
+  /** Returns project containing the edited file, if it exists. */
   def getScalaProject: Option[ScalaProject] ={
     getEditorInput() match {
       case fileEditorInput: FileEditorInput =>
