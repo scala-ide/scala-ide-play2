9e6386c4e2ca674a499f5297f9d7ac634485986e
Jedd Haberstro <jedd.haberstro@typesafe.com>
2013-06-26 15:32:39 +0200

    Added SSE template validation into proper file.

--- org.scala-ide.play2/src/org/scalaide/play2/templateeditor/TemplateStructuredEditor.scala
+++ org.scala-ide.play2/src/org/scalaide/play2/templateeditor/TemplateStructuredEditor.scala
@@ -469,65 +469,6 @@ class ScalaLineStyleProvider(prefStore: IPreferenceStore) extends AbstractLineSt
      { /* ScalaSyntaxClass instances are stored internally in the scala text regions*/ }
 }
 
-class ScalaSourceValidator extends IValidator {
-  import org.eclipse.wst.validation.internal.provisional.core.IReporter
-  import org.eclipse.wst.validation.internal.provisional.core.IValidationContext
-  
-  /* IValidator methods */
-  
-  def cleanup(report: IReporter) = {}
-  
-  def validate(helper: IValidationContext, reporter: IReporter) = {
-    import org.eclipse.core.resources.IMarker
-    import org.eclipse.core.resources.IResource
-    import org.eclipse.core.resources.ResourcesPlugin
-    import org.eclipse.core.runtime.Path
-    import org.eclipse.jdt.core.IJavaModelMarker
-    import org.eclipse.wst.validation.internal.operations.LocalizedMessage
-    import org.eclipse.wst.validation.internal.provisional.core.IMessage
-    import org.scalaide.play2.templateeditor.TemplateCompilationUnit
-    import scala.util.Try
-
-    val wsroot = ResourcesPlugin.getWorkspace().getRoot()
-    for {
-      uri <- helper.getURIs()
-      if !reporter.isCancelled()
-      currentFile <- Option(wsroot.getFile(new Path(uri)))
-      if currentFile.exists()
-      model <- Try(StructuredModelManager.getModelManager().getModelForRead(currentFile))
-    }{
-      try {
-        val markerType = IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER
-        for {
-          markers <- Try(currentFile.findMarkers(markerType, true, IResource.DEPTH_ONE))
-          marker <- markers
-        } marker.delete()
-        
-        val doc = model.getStructuredDocument()
-        val compilationUnit = TemplateCompilationUnit.fromFileAndDocument(currentFile, doc)
-        for (error <- compilationUnit.reconcile(doc.get())) {
-          val (priority, severity) =
-            if (error.isError()) (IMarker.PRIORITY_HIGH, IMarker.SEVERITY_ERROR)
-            else if (error.isWarning()) (IMarker.PRIORITY_NORMAL, IMarker.SEVERITY_WARNING)
-            else (IMarker.PRIORITY_LOW, IMarker.SEVERITY_INFO)
-
-          val marker = currentFile.createMarker(markerType)
-          marker.setAttribute(IMarker.LINE_NUMBER, doc.getLineOfOffset(error.getSourceStart()) + 1)
-          marker.setAttribute(IMarker.CHAR_START, error.getSourceStart())
-          marker.setAttribute(IMarker.CHAR_END, error.getSourceStart() + (error.getSourceEnd() - error.getSourceStart() + 1))
-          marker.setAttribute(IMarker.MESSAGE, error.getMessage())
-          marker.setAttribute(IMarker.USER_EDITABLE, java.lang.Boolean.FALSE)
-          marker.setAttribute(IMarker.PRIORITY, priority)
-          marker.setAttribute(IMarker.SEVERITY, severity)
-        }
-      }
-      finally {
-        model.releaseFromRead()
-      }
-    }
-  }
-}
-
 // According to http://www.eclipsezone.com/eclipse/forums/t73617.html#92026455 in the SourceViewer and SourceViewerConfiguration hierarchy
 // "content type" actually means "partition type"
 class TemplateStructuredTextViewerConfiguration(prefStore: IPreferenceStore, editor: TTemplateEditor) extends StructuredTextViewerConfiguration {
