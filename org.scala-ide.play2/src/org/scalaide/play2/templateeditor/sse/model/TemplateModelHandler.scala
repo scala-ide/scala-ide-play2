914294b7588776e80b3a7fe3b89a17bae96aebd2
Jedd Haberstro <jedd.haberstro@typesafe.com>
2013-06-28 16:09:58 +0200

    Removed dependency on jsp packages.

--- org.scala-ide.play2/src/org/scalaide/play2/templateeditor/sse/model/TemplateModelHandler.scala
+++ org.scala-ide.play2/src/org/scalaide/play2/templateeditor/sse/model/TemplateModelHandler.scala
@@ -1,6 +1,6 @@
 package org.scalaide.play2.templateeditor.sse.model
 
-import org.eclipse.jst.jsp.core.internal.encoding.JSPDocumentHeadContentDetector
+import org.eclipse.wst.html.core.internal.encoding.HTMLDocumentCharsetDetector
 import org.eclipse.wst.sse.core.internal.document.IDocumentCharsetDetector
 import org.eclipse.wst.sse.core.internal.document.IDocumentLoader
 import org.eclipse.wst.sse.core.internal.ltk.modelhandler.AbstractModelHandler
@@ -15,7 +15,9 @@ class TemplateModelHandler extends AbstractModelHandler() {
   setId(TemplateModelHandler.ModelHandlerID)
   setAssociatedContentTypeId(TemplateModelHandler.AssociatedContentTypeID)
 
-  override def getEncodingDetector(): IDocumentCharsetDetector = new JSPDocumentHeadContentDetector
+  // Perhaps at some point it would be beneficial to have our own documentcharsetdetector.
+  // At the moment, I don't when there would be a difference between html and template files.
+  override def getEncodingDetector(): IDocumentCharsetDetector = new HTMLDocumentCharsetDetector
   override def getDocumentLoader(): IDocumentLoader = new TemplateDocumentLoader
   override def getModelLoader(): IModelLoader = new TemplateModelLoader
 }
