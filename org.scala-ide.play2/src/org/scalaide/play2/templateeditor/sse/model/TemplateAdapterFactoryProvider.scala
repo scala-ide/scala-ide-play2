7c904528fc9eaa3d286fe83a4ec8dd9d3ba16df3
Jedd Haberstro <jedd.haberstro@typesafe.com>
2013-06-28 15:56:09 +0200

    Fixed possible null pointer when performing HTML format action.
    The format action doesn't format properly, but it doesn't prompt the user with a null pointer now :).

--- org.scala-ide.play2/src/org/scalaide/play2/templateeditor/sse/model/TemplateAdapterFactoryProvider.scala
+++ org.scala-ide.play2/src/org/scalaide/play2/templateeditor/sse/model/TemplateAdapterFactoryProvider.scala
@@ -1,18 +1,24 @@
 package org.scalaide.play2.templateeditor.sse.model
 
-import org.eclipse.wst.sse.ui.internal.provisional.registry.AdapterFactoryProvider
-import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel
-import org.eclipse.wst.sse.ui.internal.util.Assert
-import org.eclipse.wst.sse.ui.internal.contentoutline.IJFaceNodeAdapter
 import org.eclipse.wst.html.ui.internal.contentoutline.JFaceNodeAdapterFactoryForHTML
+import org.eclipse.wst.jsdt.web.core.javascript.IJsTranslation
+import org.eclipse.wst.jsdt.web.core.javascript.JsTranslationAdapterFactory
 import org.eclipse.wst.sse.core.internal.ltk.modelhandler.IDocumentTypeHandler
+import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel
+import org.eclipse.wst.sse.ui.internal.contentoutline.IJFaceNodeAdapter
+import org.eclipse.wst.sse.ui.internal.provisional.registry.AdapterFactoryProvider
+import org.eclipse.wst.sse.ui.internal.util.Assert
 
 class TemplateAdapterFactoryProvider extends AdapterFactoryProvider {
+  
   override def addAdapterFactories(structuredModel: IStructuredModel) = {
     val factoryRegistry = structuredModel.getFactoryRegistry()
     Assert.isNotNull(factoryRegistry, "Program error: client caller must ensure model has factory registry")
     if (factoryRegistry.getFactoryFor(classOf[IJFaceNodeAdapter]) == null) {
       factoryRegistry.addFactory(new JFaceNodeAdapterFactoryForHTML)
+    }
+    if (factoryRegistry.getFactoryFor(classOf[org.eclipse.wst.jsdt.web.core.javascript.IJsTranslation]) == null) {
+      factoryRegistry.addFactory(new JsTranslationAdapterFactory)
     }
   }
 
