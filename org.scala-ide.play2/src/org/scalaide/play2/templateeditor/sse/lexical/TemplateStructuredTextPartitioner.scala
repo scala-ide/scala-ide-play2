b9b5994beb14d9f8b11550d9db7192e1b6f93935
Jedd Haberstro <jedd.haberstro@typesafe.com>
2013-07-01 13:46:19 +0200

    Fixed template completion not triggering at end of partition
    The structured template partitioner needed to implement IDocumentPartitionerExtension2 to allow for "open partitions" (similarly to how the template partitioner for the older template editor did).

--- org.scala-ide.play2/src/org/scalaide/play2/templateeditor/sse/lexical/TemplateStructuredTextPartitioner.scala
+++ org.scala-ide.play2/src/org/scalaide/play2/templateeditor/sse/lexical/TemplateStructuredTextPartitioner.scala
@@ -9,8 +9,10 @@ import org.eclipse.wst.sse.core.internal.text.rules.StructuredTextPartitioner
 import org.scalaide.play2.templateeditor.TemplateSyntaxClasses
 import org.scalaide.play2.templateeditor.lexical.TemplateDocumentPartitioner
 import org.scalaide.play2.templateeditor.lexical.TemplatePartitions
+import org.eclipse.jface.text.IDocumentPartitionerExtension2
+import org.eclipse.jface.text.ITypedRegion
 
-class TemplateStructuredTextPartitioner extends StructuredTextPartitioner {
+class TemplateStructuredTextPartitioner extends StructuredTextPartitioner with IDocumentPartitionerExtension2 {
   
   private val htmlPartitioner: StructuredTextPartitionerForHTML = new StructuredTextPartitionerForHTML
   
@@ -58,5 +60,23 @@ class TemplateStructuredTextPartitioner extends StructuredTextPartitioner {
     val instance = new TemplateStructuredTextPartitioner
     instance.connect(fStructuredDocument)
     instance
+  }
+  
+  /* IDocumentPartitionerExtension2 methods */
+  
+  override def getManagingPositionCategories(): Array[String] = null
+  
+  override def getContentType(offset: Int, preferOpenPartitions: Boolean): String = 
+    getPartition(offset, preferOpenPartitions).getType()
+  
+  override def computePartitioning(offset: Int, length: Int, includeZeroLengthPartitions: Boolean): Array[ITypedRegion] =
+    computePartitioning(offset, length)
+  
+  override def getPartition(offset: Int, preferOpenPartitions: Boolean): ITypedRegion = {
+    val partition = getPartition(offset)
+    if (preferOpenPartitions && partition.getOffset() == offset && offset > 0) {
+      getPartition(offset - 1)
+    }
+    else partition
   }
 }
