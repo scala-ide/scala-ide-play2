eeb700cf7e14a839404bf2ff6510ad9c501375ee
Luc Bourlier <luc.bourlier@typesafe.com>
2013-08-13 17:35:19 +0200

    Adds real content check to the content describer
    To avoid the cases when play template editor code is used inside other
    editors.
    Fixes #176
    
    (cherry picked from commit 47c48dd2d9dbfd42a2cde141acf677c0873f8a82)

--- org.scala-ide.play2.tests/src/org/scalaide/play2/TemplateTestSuite.scala
+++ org.scala-ide.play2.tests/src/org/scalaide/play2/TemplateTestSuite.scala
@@ -1,20 +1,19 @@
 package org.scalaide.play2
 
-import org.junit.Test
-import org.junit.Before
 import org.junit.runner.RunWith
-import org.junit.Assert._
 import org.junit.runners.Suite.SuiteClasses
 import org.scalaide.play2.templateeditor.lexical.TemplatePartitionTokeniserTest
 import org.scalaide.play2.templateeditor.lexical.TemplateCompilationUnitTest
 import org.scalaide.play2.templateeditor.sse.lexical.TemplateRegionParserTest
 import org.scalaide.play2.indenter.TemplateAutoIndentTest
+import org.scalaide.play2.templateeditor.sse.TemplateContentDescriberTest
 @RunWith(value = classOf[org.junit.runners.Suite])
 @SuiteClasses(value = Array(
   classOf[TemplateAutoIndentTest],
   classOf[TemplatePartitionTokeniserTest],
   classOf[TemplateCompilationUnitTest],
-  classOf[TemplateRegionParserTest]))
+  classOf[TemplateRegionParserTest],
+  classOf[TemplateContentDescriberTest]))
 class TemplateTestSuite {
 
 }
