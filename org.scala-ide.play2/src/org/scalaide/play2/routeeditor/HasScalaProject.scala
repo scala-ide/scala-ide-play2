fab84f4faeb3f952b7cb93cefdd371e6c31a0018
Luc Bourlier <luc.bourlier@typesafe.com>
2014-08-28 15:32:32 +0200

    Adapts to new IScalaProject name and location

--- org.scala-ide.play2/src/org/scalaide/play2/routeeditor/HasScalaProject.scala
+++ org.scala-ide.play2/src/org/scalaide/play2/routeeditor/HasScalaProject.scala
@@ -1,8 +1,8 @@
 package org.scalaide.play2.routeeditor
 
-import org.scalaide.core.api.ScalaProject
+import org.scalaide.core.IScalaProject
 
 /** @note This marker trait should be moved in the scala-ide sdt.core bundle. */
 trait HasScalaProject {
-  def getScalaProject: Option[ScalaProject]
+  def getScalaProject: Option[IScalaProject]
 }
